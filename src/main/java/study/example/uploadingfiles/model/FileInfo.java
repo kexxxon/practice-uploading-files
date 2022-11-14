6170b5b1ebce8634f951e3e0ad1e47442cbba60e
kexxxon <dev.kexon@gmail.com>
2022-11-15 01:52:42 +0900

    feat: Fileinfo 구현

---
+++ src/main/java/study/example/uploadingfiles/model/FileInfo.java
@@ -0,0 +1,11 @@
+package study.example.uploadingfiles.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FileInfo {
+	private String name;
+	private String url;
+}
