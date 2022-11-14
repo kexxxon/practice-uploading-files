c903e3625de55a5a3655f08e36030f252a1c2332
kexxxon <dev.kexon@gmail.com>
2022-11-15 01:53:01 +0900

    feat: ResponseMessage 구현

---
+++ src/main/java/study/example/uploadingfiles/message/ResponseMessage.java
@@ -0,0 +1,10 @@
+package study.example.uploadingfiles.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseMessage {
+	private String message;
+}
