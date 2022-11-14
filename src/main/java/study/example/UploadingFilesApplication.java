e69d58e278f3103aa50d10f3ea87e5411840ed19
kexxxon <dev.kexon@gmail.com>
2022-11-15 01:54:24 +0900

    feat: UploadingFilesApplication 구현

--- src/main/java/study/example/UploadingFilesApplication.java
+++ src/main/java/study/example/UploadingFilesApplication.java
@@ -1,13 +1,25 @@
-package study;
+package study.example;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import study.example.uploadingfiles.service.FileStorageService;
+
 @SpringBootApplication
-public class UploadingFilesApplication {
+public class UploadingFilesApplication implements CommandLineRunner {
+	@Resource
+	FileStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UploadingFilesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
 }
